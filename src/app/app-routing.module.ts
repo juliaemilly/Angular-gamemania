9802b48e7b0976c5df2b12f7f6d170738e809572
juliaemilly <julia.elb01@gmail.com>
2022-08-19 16:22:21 -0300

    atualizacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { CarrinhoComponent } from './pages/carrinho/carrinho.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'Contact',component:ContactComponent},
+  {path:'Login', component:LoginComponent},
+  {path:'Carrinho',component:CarrinhoComponent},
+  {path:'Produtos',component:ProdutosComponent},
+  {path:'Cadastro',component:CadastroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CarrinhoComponent } from './pages/carrinho/carrinho.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    
+    LoginComponent,
+    ProdutosComponent,
+    ContactComponent,
+    CarrinhoComponent,
+    CadastroComponent,
   ],
   imports: [
     BrowserModule,
